c61953f3ec069ff2b056453db8b74e038d7040ce
Marcio Shigueru Katsumata <marcio.katsumata@totvs.com.br>
2018-04-25 15:01:18 -0300

    subida projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ThfModule } from '@totvs/thf-ui';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule,
+	AppRoutingModule,
+	ThfModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
